caee7bffdf366e15a876f4f54e0aefc0f1940995
Alejo <alejofsuarez@gmail.com>
2022-08-03 11:44:37 -0300

    ahora esta completa la clase FlaskForm

--- form.py
+++ form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 
-class Form(#Aca falta algo):
+class Form(FlaskForm):
 
 #completar lo de abajo
     name=StringField("", validators=[])
